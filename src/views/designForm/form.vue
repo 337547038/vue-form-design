9cac0225a0e95c764a7698bcc6b776b761da7c5f
337547038 <337547038@qq.com>
2021-10-31 23:24:24 +0800

    update

--- src/views/designForm/form.vue
+++ src/views/designForm/form.vue
@@ -1,4 +1,4 @@
-<!-- Created by 337547038 on 2021/9/26. -->
+<!-- Created by 337547038 on 通用表单添加修改查看页. -->
 <template>
   <div v-loading="loading">
     <design-form :formData="formData" ref="formName">
@@ -8,12 +8,10 @@
 </template>
 
 <script>
-import designForm from '@/components/form/index.vue'
+import designForm from './components/form.vue'
 import {reactive, toRefs, provide, ref} from 'vue'
 import {useRoute, useRouter} from 'vue-router'
 import {getDesignFormRow} from '@/api'
-import {evil} from "@/utils"
-import testCom from "@/views/test.vue"
 
 export default {
   name: "add",
@@ -22,35 +20,50 @@ export default {
   setup(props) {
     const route = useRoute()
     const dataSource = route.query.dataSource
-    const id = route.query.id
+    const formId = route.query.formId
     const state = reactive({
       loading: false,
       formData: {}
     })
     // 注入选项方法获取值
-    const optionsValue = ref([{label: "选项1", value: '1'}])
+    /*const optionsValue = ref([{label: "选项1", value: '1'}])
     provide("getCheckbox", optionsValue)
     // 注入自定义组件
     provide('componentName', testCom)
+    // 注入校验规则
+    provide('getMethosdd', (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('Please input the password'))
+      } else {
+        callback()
+      }
+    })
+    // 表单控件值改变事件
+    provide('controlChange', ({key, value}) => {
+      console.log(key)
+      console.log(value)
+    })*/
     // 获取表单设计数据
-    if (id) {
+    if (formId) {
       state.loading = true
-      getDesignFormRow(id)
+      getDesignFormRow(formId)
         .then(res => {
           if (res.data.code === 200) {
-            state.formData = evil(res.data.data[0].content)
+            state.formData = JSON.parse(res.data.data[0].formData)
           }
           state.loading = false
         })
     }
-    const formName=ref()
+    const formName = ref()
     const submit = () => {
+      const formValue = formName.value.getValue()
+      console.log(formValue)
       formName.value.validate((valid) => {
         console.log(valid)
         if (valid) {
-          alert('submit!')
+          alert('submit')
         } else {
-          console.log('error submit!00!')
+          console.log('error submit')
           return false
         }
       })
