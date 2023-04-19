429ebeeeae0f0bb9079c507d071f4c9f224ef040
337547038 <337547038@qq.com>
2023-04-19 22:48:47 +0800

    流程&优化

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+import diyIconfont from './diy-iconfont.vue'
+import userDialog from './userDialog.vue'
+
+export default (app: any) => {
+  app.component('DiyIconfont', diyIconfont)
+  app.component('UserDialog', userDialog)
+}
---
+++ src/views/task/applyed.vue
@@ -0,0 +1,6 @@
+<!-- Created by 337547038 我发起的 -->
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts"></script>
