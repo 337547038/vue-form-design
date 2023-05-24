9842af448792a49520b4077ac4ff3c425333b881
337547038 <337547038@qq.com>
2023-05-24 11:31:06 +0800

    TypeScript语法警告优化

---
+++ docs/assets/list-2b126cd7.js
@@ -0,0 +1,1 @@
+import{_ as t,o as e,c as o}from"./index-8ad7f075.js";const s={name:"List",components:{},props:{},data(){return{}},computed:{},mounted(){},methods:{}};function n(c,r,a,p,d,m){return e(),o("div")}const i=t(s,[["render",n]]);export{i as default};
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,34 +1,5 @@
 <template>
-  <div class="container" ref="container">
-    <div id="box" style="position: absolute; top: 0; left: 0">Move me with arrow keys!</div>
-  </div>
+  <div class="container"></div>
 </template>
 
-<script setup>
-  import { reactive, onMounted } from 'vue'
-
-  onMounted(() => {
-    const box = document.getElementById('box')
-    let left = 0,
-      top = 0
-
-    document.addEventListener('keydown', function (event) {
-      console.log(event)
-      if (event.keyCode === 37) {
-        // left arrow key
-        left--
-      } else if (event.keyCode === 38) {
-        // up arrow key
-        top--
-      } else if (event.keyCode === 39) {
-        // right arrow key
-        left++
-      } else if (event.keyCode === 40) {
-        // down arrow key
-        top++
-      }
-      box.style.left = left + 'px'
-      box.style.top = top + 'px'
-    })
-  })
-</script>
+<script></script>
