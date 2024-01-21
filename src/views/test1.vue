a30f19dbd83897f1e6ee00e4306cc3c73ef4208f
337547038 <337547038@qq.com>
2024-01-21 17:10:33 +0800

    api

--- src/views/test1.vue
+++ src/views/test1.vue
@@ -7,7 +7,11 @@
     />
   </div>
 </template>
-
+<route>
+{meta:{
+permissions:false,
+layout:'hidden'}}
+</route>
 <script setup lang="ts">
   import { ref, computed, provide } from 'vue'
   import akScreen from './design/screen/components/screen.vue'
@@ -16,18 +20,38 @@
   const screenData = ref({
     list: [
       {
-        type: 'line',
-        position: { width: 400, height: 300, left: 232, top: 91 },
+        type: 'bar',
+        position: { width: 400, height: 300, left: '86px', top: '104px' },
         option: {
           xAxis: {
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            data: []
           },
           yAxis: { type: 'value' },
           grid: { left: 0, top: 0, right: 0, bottom: 30 },
-          series: [{ data: [150, 230, 224, 218, 135, 147, 260], type: 'line' }]
+          series: [{ data: [], type: 'bar' }]
         },
-        config: { optionsType: 1, method: 'post', requestUrl: 'getdict' }
+        config: {
+          optionsType: 1,
+          method: 'get',
+          requestUrl: 'test.json'
+        },
+        events: {
+          beforeFetch: (data, route) => {
+            // data请求参数，route当前路由信息
+            console.log('beforeFetch', data)
+            return data
+          },
+          afterFetch: (res, data) => {
+            // res响应数据, 当前组件数据data
+            console.log('当前组件数据', data)
+            console.log('接口响应数据', res)
+            //下面两行是重点，将接口返回的数据赋值级当前组件
+            data.xAxis.data = res.xAxis
+            data.series[0].data = res.series
+            return data //返回新的图表数据
+          }
+        }
       }
     ],
     config: {
